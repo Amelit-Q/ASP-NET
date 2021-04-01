df0846b6c0eab30bae5b42736650fdb7745a8373
Amelit <muradovt111@gmail.com>
2021-04-01 22:02:20 +0300

    For now just testing required fields

--- ASP-first-try/Models/Ticket.cs
+++ ASP-first-try/Models/Ticket.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_first_try.Models
 {
     public class Ticket
     {
         public int TicketId { get; set; }
+        [Required]
+        [FromBody]
         public int ProjectId { get; set; }
+        [Required]
         [FromBody]
         public string Title { get; set; }
         [FromBody]
         public string Description { get; set; }
     }
-}
+} 
