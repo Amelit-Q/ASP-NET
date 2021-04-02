46ffef2247ffe730d17e87d9ab9c7858bcc67770
p.tretyakov <p.tretyakov@simplex-software.ru>
2021-04-02 20:54:46 +0300

    Added data validation plus argument for DateTime in Ticket.cs

--- ASP-first-try/Models/Ticket.cs
+++ ASP-first-try/Models/Ticket.cs
@@ -1,18 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ASP_first_try.ModelValidations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_first_try.Models
 {
     public class Ticket
     {
-        public int TicketId { get; set; }
-        [Required]
-        [FromBody]
-        public int ProjectId { get; set; }
-        [Required]
-        [FromBody]
-        public string Title { get; set; }
-        [FromBody]
-        public string Description { get; set; }
+        [FromBody] public int? TicketId { get; set; }
+        [Required] [FromBody] public int? ProjectId { get; set; }
+        [Required] [FromBody] public string Title { get; set; }
+        [FromBody] public string Description { get; set; }
+        public string Owner { get; set; }
+
+        [Ticket_EnsureDueDateTicketOwner] public DateTime? DueDate { get; set; }
     }
-} 
+}
